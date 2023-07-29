70d0dc6865b40fa0fc71a6cbd34c4e172f31cf78
njine <brikab@gmail.com>
2023-07-29 05:54:03 +0300

    hl

--- iko.c
+++ iko.c
@@ -1,72 +1,74 @@
 #include "shell.h"
 
 /**
-**_strncpy -  string copy
-*@dest: destination the string to be copied
-*@src: where the string originates
-*@n: the no of characters to be replicated
-*Return: the concatenated string
-*/
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
-char *s = dest;
+	int i, j;
+	char *s = dest;
 
-i = 0;
-for (int i = 0; src[i] != '\0' && i < n - 1; i++)
-{
-dest[i] = src[i];
-}
-if (i < n)
-{
-for (int j = i; j < n; j++)
-{
-dest[j] = '\0';
-j++;
-}
-}
-return (s);
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
-**_strncat - concatenates 2 strings
-*@dest: 1st string
-*@src:  2nd string
-*@n: bytes to be  used maximally
-*Return: concatenated string
-*/
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
-char *s = dest;
+	int i, j;
+	char *s = dest;
 
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-if (j < n)
-dest[i] = '\0';
-return (s);
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
 
 /**
-**_strchr - locates a character in string
-*@s:  string to be parsed
-*@c:  character to search
-*Return: (s) a pointer to the memory area s
-*/
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
 char *_strchr(char *s, char c)
 {
-do {
-if (*s == c)
-return (s);
-} while (*s++ != '\0');
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
 
-return (NULL);
+	return (NULL);
 }
